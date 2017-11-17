1823492b7465afb4903ee1b993e98cbd1357a473
FatmaNader <31704640+FatmaNader@users.noreply.github.com>
2017-11-17 13:57:56 -0800

    final

--- registeration.php
+++ registeration.php
@@ -1,69 +1,153 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = "registeration";
+$conn = new mysqli($host, $user, $pass,$dbname);
+
+
+//check duplicate username 
+if(isset($_POST['user_name']))
+{
+ $name=$_POST['user_name'];
+
+ $checkdata=" SELECT `username` FROM `user` WHERE `username`='$name' ";
+
+  $get_username=$conn->query($checkdata);
+
+	   if($get_username->num_rows > 0 ){   //Indicates username failure
+
+  echo "User Name Already Exist";
+ }
+ else
+ {
+  echo "Valid";
+ }
+ exit();
+}
+
+//check duplicate email
+if(isset($_POST['Email']))
+{
+ $Email=$_POST['Email'];
+
+ $checkEmail=" SELECT `email` FROM `user` WHERE `email`='$Email' ";
+
+  $get_Email=$conn->query($checkEmail);
+
+	   if($get_Email->num_rows > 0 ){   //Indicates username failure
+
+  echo "NotValid";
+ }
+ else
+ {
+  echo "Valid";
+ }
+ exit();
+}
+
+
+if(isset($_POST['name'])&&isset($_POST['pass']))
+{
+ $name=$_POST['name'];
+ $pass=$_POST['pass'];
+ $pass =md5($pass);
+ 
+ $checkpassword=" SELECT * FROM `user` WHERE `username`='$name'&& `password`='$pass'";
+ 
+
+  $getpassword=$conn->query($checkpassword);
+
+	   if($getpassword->num_rows < 1 ){   //Indicates username failure
+
+  echo "User Name or password do not match";
+ }
+ else
+ {
+  echo "Valid";
+ }
+ exit();
+}
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-body {
-    background-color:lightblue ;
-	font-size: 40px;
-}
 
-h2 {
-    color: white;
-    text-align: center;
-}
-
-label {
-	color:green;
-    font-family: verdana;
-    font-size: 20px;
-}
-</style>
 <title> Registeration system  </title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
 <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
 <script src="validate.js"></script>
-<link rel="stylesheet" type="text/css" href="mystyle.css">
+<link rel="stylesheet" type="text/css" href='css/registeration_css.css'>
+<script type="text/javascript" src="js/jquery.js"></script>
+
+
+
+
 </head>
+
 <body>
 
 <div name ="sign_up" class="sign_up">
   <h2> Sign Up </h2>
-  <form method="post" onsubmit= "valid_sign_up()" action="submit.php" name="sign_up_form" id="sign_up_form">
+  <div class="container">
+    <div class="left"></div>
+    <div class=right>
+      <div class="formbox">
+     <form id="sign_up_form" action="submit.php" method="post" onsubmit="return valid_sign_up();"  name="sign_up_form"  >
     <div class="sign_up_input" name="sign_up_input">
   <label> Username </label>
-  <input type="text" name="Name">
+  <br>
+  <input type="text" name="Name" id= "Name_sign_up" onkeyup="checkname();">
+   <span  id="name_status"></span>
   <br>
   <label> Email</label>
-  <input type="text" name="email">
+  <br>
+  <input type="text" name="email" id=Email onkeyup="checkEmail();">
+  <span id="Email_status"></span>
   <br/>
-  <label> Password </label>
-  <input type="password" name="password">
+  <label> Password </label> 
+   <br>
+  <input type="password" name="password" id="password1">
   <br>
   <label> Confirm password </label>
-  <input type="password" name="password_1">
   <br>
+  <input type="password" name="password_1" id="confirmPassword" onkeyup="confirmPass();" >
+  <span   id="passwordCheck" ></span>
+  <br>
+  <div id="buttons">
+  <input id="submit_btn" type="submit" value="submit">
+  <button id="sign_in_btn" type="button"> Sign in </button>
 </form>
-<div id="buttons">
-<button id="sign_in_btn" type="button"> Sign in </button>
-<button id="submit_btn" type="submit"> Submit </button>
+</div>
+</div>
+</div>
 </div>
 </div>
 </div>
 
 <div class="sign_in" name="sign_in" >
   <h2>Sign in </h2>
-<form method="post" action="login.php" id="sign_in_form">
+  <div class="sign_in_container">
+    <div class="sign_in_left"></div>
+    <div class="sign_in_right">
+<form method="post" action="login.php" id="sign_in_form" onsubmit="return valid_sign_in();"  >
   <label> Username </label>
-  <input type="text" name="Name">
+  <input type="text" name="Name" id = "Name">
+     <span  id="name_status1"></span>
   <br>
   <label> Password </label>
-  <input type="password" name="password">
+  <input type="password" name="password" id="password" onkeyup="checkpassword();">
+     <span  id="password_status"></span>
+	 
   <br>
   <input type="submit" value="sign in">
   <br/>
 </form>
+</div>
 </div>
 
 <script>
@@ -75,17 +159,7 @@ $(document).ready(function(){
 });
 });
 
-$( "#sign_in_form" ).validate({
-  rules: {
-    Name: {
-      required: true
-    }
-, password: {
-  required: true
-}
 
-  }
-});
 </script>
 
 
--- validate.js
+++ validate.js
@@ -1,6 +1,8 @@
 $(function(){
   //validating the sign_up_form
 $( "#sign_up_form" ).validate({
+  errorClass: "my-error-class",
+   validClass: "my-valid-class",
   rules: {
     Name: {
       required: true,
@@ -13,17 +15,181 @@ $( "#sign_up_form" ).validate({
 
   }
 });
+});
+
+$( "#sign_in_form" ).validate({
+  rules: {
+    Name: {
+      required: true
+    }
+, password: {
+  required: true
+}
+
+  }
+});
+
 //function for checking if the form is valid or not
-function valid_sign_up(){
-  if(($( "#sign_up_form" ).valid())==true) {
-    alert("true");
-  return true;}
+function valid_sign_in(){
+	var pass=document.getElementById( "password_status" ).innerHTML;
+  if(($( "#sign_in_form" ).valid())==true&& pass =="Valid") {
+   
+  return true;
+  }
     else
-    {   alert("false");
+    {   alert ("username and password do not match");
       return false;
 }
 }
+ 
+
+//function for checking if the form is valid or not
+function valid_sign_up(){
+	var name=document.getElementById( "name_status" ).innerHTML.trim();
+	var pass=document.getElementById( "passwordCheck" ).innerHTML.trim();
+	var Email=document.getElementById( "Email_status" ).innerHTML.trim();
+
+  if(($( "#sign_up_form" ).valid())==true && (name=="Valid") &&(pass=="passwordsMatch" ) && (Email!="NotValid")){
+
+  return true;
+  }
+  else if(pass!="passwordsMatch")
+  {
+	  alert("Passwords do not match");
+	  return false;
+  }
+    else
+    {   
+    alert("please check your informaion");
+      return false;
+}
+}
+
+//mo7awlaa fashaaa lel login
+function checkpassword ()
+{
+ var name=document.getElementById( "Name" ).value;
+ var pass=document.getElementById( "password" ).value; 
+ if(pass&&name)
+ {
+  $.ajax({ 
+  type: 'post',
+  url: 'registeration.php',
+  data: {
+   name:name,
+   pass:pass,
+  },
+  success: function (response) {
+   $( '#password_status' ).html(response);
+   
+   if(response=="Valid")	
+   {
+	   
+    return true;	
+   }
+   else
+   {
+	     $( '#password_status' ).html("NotValid");
+    return false;	
+   }
+  }
+  });
+ 
+ }
+ else
+ {
+  $( '#password_status' ).html("wrong");
+  return false;
+ }
+ return true;
+}
+
+
+
 //validating the sign in form
+function checkname()
+{
+ var name=document.getElementById( "Name_sign_up" ).value;
+ if(name)
+ {
+  $.ajax({ 
+  type: 'post',
+  url: 'registeration.php',
+  data: {
+   user_name:name,
+  },
+  success: function (response) {
+   $( '#name_status' ).html(response);
+   if(response=="Valid")	
+   {
+    return true;	
+   }
+   else
+   {
+	   $( '#name_status' ).html("NotValid");
+    return false;	
+   }
+  }
+  });
+ }
+ else
+ {
+  $( '#name_status' ).html("NotValid");
+  return false;
+ }
 
+}
+//validating two passwords match
+function confirmPass()
+{
+	var pass=document.getElementById( "password1" ).value;
+	var confirmPass=document.getElementById( "confirmPassword" ).value;
+	if(pass==confirmPass)
+	{ 
+       
+		$( '#passwordCheck' ).html("passwordsMatch");
+		 return true;
+	}
+	else 
+	{
+		$( '#passwordCheck' ).html("");
+		return false;
+	}
+	
+}
 
-});
+function checkEmail()
+{
+ var Email=document.getElementById( "Email" ).value;
+ if(Email)
+ {
+  $.ajax({ 
+  type: 'post',
+  url: 'registeration.php',
+  data: {
+   Email:Email,
+  },
+  success: function (response) {
+   $( '#Email_status' ).html(response);
+   if(response=="NotValid")	
+  {
+	   $( '#Email_status' ).html("NotValid");
+    return false;	
+   }
+   else
+	   {
+		     $( '#Email_status' ).html(" ");
+    return true;	
+   }
+   
+  }
+  });
+ }
+ else
+ {
+  $( '#Email_status' ).html(" ");
+  return false;
+ }
+
+}
+
